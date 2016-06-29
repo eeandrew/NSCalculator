19d72bfe00f58be8c4abc1c65e0fa430cfaf510d
zhoulin <zhoulin@zhoulin900.local>
2016-06-29 18:03:32 +0800

    add explain

--- app/app.component.js
+++ app/app.component.js
@@ -7,7 +7,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: "calculator",
-            template: "\n    <GridLayout rows=\"auto,*\" columns=\"*\">\n     <StackLayout row=\"0\" style=\"font-size:60;color:#fff;height:120;background-color:rgba(0,0,0,0.8);text-align:right;vertical-align:bottom;\">\n      <Label text=\"99\" ></Label>\n     </StackLayout>\n     <GridLayout row=\"1\" style=\"background-color:#fff;text-align:center;\">\n      <Label text=\"keyboards\" ></Label>\n      </GridLayout>\n    </GridLayout>\n",
+            template: "\n    <GridLayout rows=\"auto,*\" columns=\"*\">\n     <!--Dispalyer-->\n     <StackLayout row=\"0\" style=\"font-size:60;color:#fff;height:120;background-color:rgba(0,0,0,0.8);text-align:right;vertical-align:bottom;\">\n      <Label text=\"99\" ></Label>\n     </StackLayout>\n     <!--Keyboard-->\n     <GridLayout row=\"1\" style=\"background-color:#fff;text-align:center;\">\n      <Label text=\"keyboards\" ></Label>\n      </GridLayout>\n    </GridLayout>\n",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,9 +6,11 @@ import {
     selector: "calculator",
     template: `
     <GridLayout rows="auto,*" columns="*">
+     <!--Dispalyer-->
      <StackLayout row="0" style="font-size:60;color:#fff;height:120;background-color:rgba(0,0,0,0.8);text-align:right;vertical-align:bottom;">
       <Label text="99" ></Label>
      </StackLayout>
+     <!--Keyboard-->
      <GridLayout row="1" style="background-color:#fff;text-align:center;">
       <Label text="keyboards" ></Label>
       </GridLayout>
